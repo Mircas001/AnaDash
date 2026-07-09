d4cd1699107c3e42ccff53b0af62d7c107da661a
Mircas001 <mircas001@protonmail.com>
2026-07-09 16:22:27 -0300

    Barebones driver giving CPU, RAM and SWAP

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -13,16 +13,7 @@ fn main() {
     let mut sys = System::new_with_specifics(refresh_kind);
     sys.refresh_all();
 
-    let total_memory: f32 = sys.total_memory() as f32;
-
-    let total_swap: f32  = sys.total_swap() as f32;
-
-    let total_memoryby100: f32 = total_memory * 100.0;
-    let total_swapby100: f32 = total_swap * 100.0;
-
     const DAC_MAX: f32 = 4095.0;
-    let total_memory_dac: f32 = total_memory * DAC_MAX;
-    let total_swap_dac: f32 = total_memory * DAC_MAX;
 
     let mut components = Components::new_with_refreshed_list();
     for component in components.list_mut() {
@@ -30,21 +21,25 @@ fn main() {
         println!("{component:?}");
     }    
     
-    let millis = time::Duration::from_millis(1000);
+    let millis = time::Duration::from_millis(100);
 
     thread::sleep(millis);
 
     loop {
         sys.refresh_all();
-        let mem_usage_percent: f32 = sys.used_memory() as f32 / total_memory * 100.0;
-        let swap_usage_percent: f32  = sys.used_swap() as f32 / total_memory * 100.0;
+        sys.refresh_cpu_usage();
+        let mem_usage_percent: f32 = (sys.used_memory() as f32 / sys.total_memory() as f32) * 100.0;
+        let swap_usage_percent: f32  = (sys.used_swap() as f32 / sys.total_swap() as f32) * 100.0;
 
-        let mem_dac: u16 = ((sys.used_memory() as f32 / total_memory_dac).clamp(0.0, DAC_MAX) as u16);
-        let mem_dac: u16 = ((sys.used_swap() as f32 / total_swap_dac).clamp(0.0, DAC_MAX) as u16);
+        let mem_dac: f32 = (sys.used_memory() as f32 / sys.total_memory() as f32) * DAC_MAX;
+        let swap_dac: f32 = (sys.used_swap() as f32 / sys.total_swap() as f32) * DAC_MAX;
+        let cpu_dac: f32 = (sys.global_cpu_usage() * 4095.0) / 100.0;
 
-        println!("Memory Usage: {:.2}% DAC:{}", mem_usage_percent, mem_dac);    
-        println!("Swap Usage: {:.2}% DAC:{}", swap_usage_percent, mem_dac);
+        println!("Memory Usage: {}/{} {:.2}% DAC:{}", sys.used_memory(), sys.total_memory(), mem_usage_percent, mem_dac);    
+        println!("Swap Usage: {}/{} {:.2}% DAC:{}", sys.used_swap(), sys.total_swap(), swap_usage_percent, swap_dac);
+        println!("CPU Usage {}% DAC:{}", sys.global_cpu_usage(), cpu_dac);
         println!("--------------");
 
+        std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
     }
 }
