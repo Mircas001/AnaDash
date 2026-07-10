fca85a6a62ff7ce2e27f7eb004236d2e4e0f14a8
Mircas001 <mircas001@protonmail.com>
2026-07-10 19:17:25 -0300

    some demo form of an driver

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -2,30 +2,33 @@ use std::thread;
 use std::time::{Duration, Instant};
 use sysinfo::{CpuRefreshKind, MemoryRefreshKind, RefreshKind, System};
 
+use crate::sensor_utils::live_clock;
+
 mod sensor_utils;
 
 fn main() {
-    let setup_start = Instant::now();
+    #[cfg(not(target_family = "unix"))]
+    compile_error!("Only unix systems are supported!");
+
+    #[cfg(not(target_os = "linux"))]
+    println("A non-linux Unix system has been detected, this might not work!");
+
+    let one_second = Duration::new(1, 0); // * Thread requires the duration type for some reason
 
     let refresh_kind = RefreshKind::nothing()
         .with_memory(MemoryRefreshKind::everything())
         .with_cpu(CpuRefreshKind::nothing().with_cpu_usage());
 
-    let mut sys = System::new_with_specifics(refresh_kind);
+    let mut sys: System = System::new_with_specifics(refresh_kind);
 
     sys.refresh_specifics(refresh_kind);
 
     let total_memory_float: f32 = sys.total_memory() as f32;
     let total_swap_float: f32 = sys.total_swap() as f32;
 
-    let setup_time = setup_start.elapsed().as_micros();
-
-    let one_second = Duration::new(1, 0);
-
-    println!("Setting up took {}μs", setup_time);
     loop {
-        let start_time = Instant::now();
-
+        println!("{}", sensor_utils::live_clock());
+        // this will send the current time every second to the resource monitor
         sys.refresh_specifics(refresh_kind);
         sys.refresh_cpu_usage();
 
@@ -33,11 +36,9 @@ fn main() {
         let swap_used: f32 = sys.used_swap() as f32 / total_swap_float;
         let cpu_load: f32 = sys.global_cpu_usage();
 
-        let time_taken: u128 = start_time.elapsed().as_micros();
-
         let cpu_temp: u32 = sensor_utils::read_cpu_temp();
 
-        println!("Running this took {}μs", time_taken);
+        // * For now, we are just printing the data we get, but I'll have to figure out how to send it over usb, later.
         println!(
             "Memory:{:.2} | Swap:{:.2} | Cpu Load:{:.2} | Cpu Temp:{}C",
             mem_used * 100.0,
