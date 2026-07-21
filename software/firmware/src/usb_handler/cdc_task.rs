5207513dba78d64e989d35776e6d62b2f54f0425
Mircas001 <mircas001@protonmail.com>
2026-07-21 17:55:35 -0300

    finish input task, add dependencies for  cdc

--- software/firmware/src/usb_handler/cdc_task.rs
+++ software/firmware/src/usb_handler/cdc_task.rs
@@ -5,4 +5,9 @@ use embassy_usb::class::cdc_acm::CdcAcmClass;
 #[embassy_executor::task]
 pub async fn cdc_task(mut cdc: CdcAcmClass<'static, Driver<'static, USB>>) {
     // it gets data from the host and sends them over to main, either being an ST7735 or mcp4728, probably main?
+    // I have no idea how to get data out, ill just get data in for now
+    // * Also, since this is USB powered, we don't care about disconnection, cuz we'll just shut down
+    // * But i'll folow the embassy example of doing the 
 }
+
+
--- software/firmware/src/usb_handler/input_task.rs
+++ software/firmware/src/usb_handler/input_task.rs
@@ -1,7 +1,9 @@
 use crate::hardware::input_handler::KeyInputs;
+use cortex_m::peripheral::SCB;
 use defmt::warn;
 use embassy_futures::select::{Either, select, select_array};
 use embassy_rp::peripherals::USB;
+use embassy_rp::rom_data::reset_to_usb_boot;
 use embassy_rp::usb::Driver;
 use embassy_time::{Duration, Timer};
 use embassy_usb::class::hid::HidWriter;
@@ -29,6 +31,8 @@ const VOLUME_UP_REPORT: MediaKeyboardReport = MediaKeyboardReport {
 const VOLUME_DOWN_REPORT: MediaKeyboardReport = MediaKeyboardReport {
     usage_id: MediaKey::VolumeDecrement as u16,
 };
+const RESET_COMBO: u8 = 0b1000_0001;
+const BOOT_COMBO: u8 = 0b0001_1000;
 
 #[embassy_executor::task]
 pub async fn input_task(
@@ -58,38 +62,52 @@ pub async fn input_task(
         match select(key_pressed_fut, encoder_turned).await {
             Either::First(_) => {
                 Timer::after(DEBOUNCE_TIME).await; // * We wait a while so we get an stable signal of LOW
-                let is_k_pressed: u8 = (inputs.key1.is_low() as u8) << 0 // * We save up 56 bits/7 bytes this way! But i should maybe think of implementing as a custom type
-                    | (inputs.key2.is_low() as u8) << 1
-                    | (inputs.key3.is_low() as u8) << 2
-                    | (inputs.key4.is_low() as u8) << 3
-                    | (inputs.key5.is_low() as u8) << 4
-                    | (inputs.key6.is_low() as u8) << 5
-                    | (inputs.key7.is_low() as u8) << 6
-                    | (inputs.key8.is_low() as u8) << 7;
-
-                let is_encoder_pressed: bool = inputs.enc_sw.is_low();
-                if is_encoder_pressed {
-                    match media_w.write_serialize(&MUTE_REPORT).await {
-                        Ok(()) => continue, // * There's no reason for you to press the encoder switch and another key
-                        Err(e) => warn!("Error sending media report!: {}", e),
+                {
+                    // * Putting it on it's own scope so it can be quickly dropped
+                    let is_encoder_pressed: bool = inputs.enc_sw.is_low();
+                    if is_encoder_pressed {
+                        match media_w.write_serialize(&MUTE_REPORT).await {
+                            Ok(()) => continue, // * There's no reason for you to press the encoder switch and another key
+                            Err(e) => warn!("Error sending media report!: {}", e),
+                        }
+                        continue; // * just reinforcing the previous point
                     }
                 }
                 {
-                    // * Putting it on it's own scope so it can be quickly dropped
+                    // * there's no reason for this to be in scope, the values are getting dropped as soon as it's over anyway,
+                    // * i just did that because it would be ugly
+                    let is_k_pressed: u8 = (inputs.key1.is_low() as u8) << 0 // * We save up 56 bits/7 bytes this way! But i should maybe think of implementing as a custom type
+                        | (inputs.key2.is_low() as u8) << 1
+                        | (inputs.key3.is_low() as u8) << 2
+                        | (inputs.key4.is_low() as u8) << 3
+                        | (inputs.key5.is_low() as u8) << 4
+                        | (inputs.key6.is_low() as u8) << 5
+                        | (inputs.key7.is_low() as u8) << 6
+                        | (inputs.key8.is_low() as u8) << 7;
+
+                    if is_k_pressed & RESET_COMBO == RESET_COMBO {
+                        SCB::sys_reset();
+                    } // * lowk wish there was a way that didnt involve it being inside the input_task
+                    if is_k_pressed & BOOT_COMBO == BOOT_COMBO {
+                        reset_to_usb_boot(0, 0);
+                    }
+
                     let mut keycodes: [u8; 6] = [0; 6];
 
                     let mut idx: usize = 0;
                     'outer: for bit in 0..8 {
                         // * Special way of iterating to accomodate the memory saving
                         if is_k_pressed & (1 << bit) != 0 {
+                            // * fancy way of saying: is the key pressed?
                             if idx < keycodes.len() {
                                 keycodes[idx] = KEYCODES[bit];
                                 idx += 1;
                             } else {
-                                break 'outer; // * Yes, it stops after 6
+                                break 'outer; // * Yes, it stops after 6, for safety.
                             }
                         }
                     }
+
                     let keys_report = KeyboardReport {
                         modifier: 0,
                         reserved: 0,
@@ -98,7 +116,7 @@ pub async fn input_task(
                     };
                     match macro_w.write_serialize(&keys_report).await {
                         Ok(()) => {}
-                        Err(e) => warn!("Error sending macro report!: {}", e),
+                        Err(e) => warn!("Error sending macro report!: {} :(", e),
                     }
                 }
             }
