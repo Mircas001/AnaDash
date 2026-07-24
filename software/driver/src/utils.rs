97f3c2a09abcdf65a78cadc605dc8ebc4efd1558
Mircas001 <mircas001@protonmail.com>
2026-07-23 22:02:12 -0300

    remove unnecessary import

--- software/driver/src/utils.rs
+++ software/driver/src/utils.rs
@@ -1,6 +1,6 @@
 use chrono::Local;
 use tokio_serial::ErrorKind::NoDevice;
-use tokio_serial::{Error, SerialPortInfo, SerialPortType, UsbPortInfo, available_ports};
+use tokio_serial::{Error, SerialPortInfo, SerialPortType, available_ports};
 
 pub fn live_clock() -> heapless::String<10> {
     let now = Local::now();
