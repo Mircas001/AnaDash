0011c24a6f43f3a670a08fc75393d65a6fc9f83d
Mircas001 <mircas001@protonmail.com>
2026-07-26 21:33:22 -0300

    add notifications to display

--- software/firmware/src/display.rs
+++ software/firmware/src/display.rs
@@ -1,19 +1,24 @@
 use embassy_rp::gpio::Output;
 use embassy_rp::peripherals::SPI1;
-use embassy_rp::spi::{Blocking as BlockingSpi, Config as SpiConfig, Spi};
-use embassy_time::Delay;
+use embassy_rp::pio_programs::ws2812::Rgb;
+use embassy_rp::spi::{Blocking as BlockingSpi, Spi};
+use embassy_time::{Delay, Instant};
+use embedded_graphics::prelude::*;
 use embedded_graphics::{
-    mono_font::{MonoTextStyle, ascii::FONT_8X13},
+    image::Image,
+    mono_font::{MonoTextStyle, ascii::FONT_6X13, ascii::FONT_8X13_BOLD},
     pixelcolor::Rgb565,
     prelude::*,
-    primitives::{Line, PrimitiveStyle},
+    primitives::{Line, PrimitiveStyle, Rectangle},
     text::{Alignment, Text},
 };
 use embedded_hal_bus::spi::ExclusiveDevice;
+use embedded_iconoir::prelude::*;
 use embedded_layout::View;
+use embedded_layout::{layout::linear::LinearLayout, prelude::*};
 use heapless::String;
+use shared::{DashboardData, NotificationData};
 use st7735_lcd::ST7735;
-use tinybmp::CompressionMethod::Rgb;
 
 pub struct Display {
     pub display: ST7735<
@@ -21,7 +26,14 @@ pub struct Display {
         Output<'static>,
         Output<'static>,
     >,
+    time: String<10>,
+    noti_buffer: NotificationData,
+    noti_cooldown: Instant,
+    display_area: Rectangle,
 }
+
+static standard_style: MonoTextStyle<'_, Rgb565> = MonoTextStyle::new(&FONT_6X13, Rgb565::WHITE);
+static bold_style: MonoTextStyle<'_, Rgb565> = MonoTextStyle::new(&FONT_8X13_BOLD, Rgb565::WHITE);
 
 impl Display {
     pub fn new(
@@ -33,23 +45,42 @@ impl Display {
         let display_spi = ExclusiveDevice::new(spi, cs, Delay).unwrap();
         let mut display = ST7735::new(display_spi, dc, rst, true, false, 160, 180);
         display.init(&mut Delay);
-        Self { display: display }
+
+        let time: String<10> = String::try_from("??:??:??").unwrap();
+
+        let noti_cooldown = Instant::now();
+
+        let display_area = display.bounding_box();
+
+        Self {
+            display: display,
+            time: time,
+            noti_cooldown: noti_cooldown,
+            noti_buffer: NotificationData::new(),
+            display_area: display_area,
+        }
     }
-    pub fn draw_status_bar(&mut self, time: String<10>) {
+    fn update_screen(&mut self, dash: DashboardData) {
+        self.time = dash.time;
+        if Instant::now() > self.noti_cooldown {
+            // * do something
+        } else {
+            self.draw_notification();
+        }
+    }
+    fn draw_status_bar(&mut self) {
         let mut clock = Text::with_alignment(
-            time.as_str(),
-            Point::new(self.display.bounding_box().center().x, 0),
-            MonoTextStyle::new(&FONT_8X13, Rgb565::WHITE),
+            self.time.as_str(),
+            Point::new(self.display_area.center().x, 0),
+            standard_style,
             Alignment::Center,
         );
-
-        let center = self.display.bounding_box().center().x_axis();
 
         let clock_dimensions = clock.bounding_box();
         Line::new(
             Point::new(0, clock_dimensions.size.height as i32),
             Point::new(
-                self.display.bounding_box().size.width as i32,
+                self.display_area.size.width as i32,
                 clock_dimensions.size().height as i32,
             ),
         )
@@ -57,5 +88,28 @@ impl Display {
         .draw(&mut self.display);
 
         clock.draw(&mut self.display);
+    }
+    pub fn show_notification(&mut self, noti: NotificationData) {
+        self.noti_buffer = noti;
+        self.noti_cooldown = Instant::from_secs(5);
+        self.draw_notification();
+    }
+    fn draw_notification(&mut self) {
+        self.display.clear(Rgb565::BLACK);
+        self.draw_status_bar();
+        let icon = icons::size24px::communication::BellNotification::new(Rgb565::WHITE);
+        let noti_icon = Image::new(&icon, Point::zero());
+        let summary = Text::new(self.noti_buffer.summary.as_str(), Point::zero(), bold_style);
+        let body = Text::new(
+            self.noti_buffer.body.as_str(),
+            Point::zero(),
+            standard_style,
+        );
+        LinearLayout::vertical(Chain::new(noti_icon).append(summary).append(body))
+            .with_alignment(horizontal::Center)
+            .arrange()
+            .align_to(&self.display_area, horizontal::Center, vertical::Center)
+            .draw(&mut self.display)
+            .unwrap();
     }
 }
